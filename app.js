3cb3d58533de7e5973e9b47cc882d8db6ffb6754
sudhir <sudhir@library-intra.myDNSname.org>
2019-10-22 12:23:58 +0530

    Heroku port added

--- app.js
+++ app.js
@@ -1,10 +1,15 @@
 const express = require('express')
 const app = express()
- 
+const port = process.env.PORT || 3000;
+
 app.get('/', function (req, res) {
   res.send('Hello World')
 })
 console.log('Server is up at 3000');
 console.log('open localhost:3000');
- 
-app.listen(3000)	
+
+
+app.listen(port, () => {
+    console.log(`server is up on port ${port}`);
+});
+
